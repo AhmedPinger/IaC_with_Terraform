934fe92165c7c63abbb5f82e8077351c1d5d6e0e
Ahmed Pinger <90968663+AhmedPinger@users.noreply.github.com>
2023-12-29 16:24:21 +0500

    Delete terraform directory

--- terraform/aws_instance/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the created EC2 instance"
-  value       = aws_instance.web_server_instance.id
-}
-
-output "public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.web_server_instance.public_ip
-}
--- terraform/aws_vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  description = "ID of the created VPC"
-  value       = aws_vpc.main.id
-}
-
-output "subnet_ids" {
-  description = "IDs of the created subnets"
-  value       = aws_subnet.subnet[*].id
-}
